9b7a92ffe6a77eaadbb7ea8bac189af85f9c6015
Muhammad Yousuf <159893294+Yousuf8521@users.noreply.github.com>
2024-03-21 19:50:45 +0500

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,244 @@
+//================================= Hello World =================================
+
+//console.log("Hello World");
+//console.log("Muhammad Yousuf");
+//console.log("03368440512");
+
+//let message="Hello World"
+//console.log(message);
+
+//let message = "hello world"
+//message = "Muhammad Yousuf"
+//console.log(message);
+
+//let message = "hello world"
+//message = "03368440512"
+//console.log(message);
+
+//===============================Var==================================
+
+//var message = "hello world"
+//console.log(message);
+
+//var message = "Muhammad Yousuf"
+//console.log(message);
+
+//var message = "03368440512"
+//console.log(message);
+
+//=================================Const=================================
+
+//const author = "hello world"
+//console.log(author)
+
+//==========================Bolean====================================
+
+//let username : string = "Muhammad Yousuf"
+//let age: number = 31;
+//let isstudent: Boolean = true;
+
+//===================Additional===================================
+//let num1= 2
+//let num2= 1
+//let num3= 4
+//console.log(num1+num2+num3)
+
+
+//=======================Subtraction==============================
+//let num1= 2
+//let num2= 1
+//let num3= 4
+//console.log(num1-num2-num3)
+
+//let num1= "Muhammad"
+//let num2= "Yousuf"
+//let num3= "03368440512"
+//console.log(num1 + num2 + num3);
+
+//let num1= "Muhammad"
+//let num2= "Yousuf"
+//let num3= "03368440512"
+//console.log(num1 - num2 - num3);
+
+
+//let a=5
+//let b=2
+//console.log(a/b);
+
+//let a=5
+//let b=2
+//console.log(a*b);
+
+//let a=5
+//let b=2
+//console.log(a/b);
+
+
+//================== BigNumber ===========================
+
+//let bignumber: Number = 999999999999999;
+//console.log(bignumber);
+
+
+//let bignumber: Number = 9999999999999999999;
+//console.log(bignumber);
+
+
+//let firstName : String = "Muhammad";
+//let lastName : String = "Yousuf";
+
+//let fullName : String = firstName + lastName;
+
+//console.log(fullName);
+
+
+//let firstName : String = "Muhammad";
+//let middleName : String = "Yousuf";
+//let lastName : String = "Saleem";
+
+//let fullName : String = firstName + " " + middleName + " " + lastName;
+
+//console.log(fullName);
+
+
+//=======================Template Literals===============================
+
+//let firstName="Muhammad Yousuf"
+//let mobileNumber= "03368440512"
+//console.log(firstName + " " mobileNumber)
+
+
+//let firstName="Muhammad Yousuf"
+//let mobileNumber= "03368440512"
+//console.log(`My Name is ${firstName}and Mobile Number is ${mobileNumber}`);
+
+
+//====================Unary Operator===================================
+
+//let index=10
+//++index //10+1
+//++index //10+2
+//++index //10+3
+//++index //10+4
+//++index //10+5
+
+//console.log(index);
+
+//let index=10
+//++index //10+1
+//--index //10-2
+//++index //10+3
+//++index //10+4
+//--index //10-5
+
+//console.log(index);
+
+
+//let index=12
+//++index //12+1
+//--index //12-2
+//++index //12+3
+//++index //12+4
+//--index //12-5
+
+//console.log(index);
+
+
+//let index=12
+//++index //12+1
+//++index //12+2
+//++index //12+3
+//++index //12+4
+//++index //12+5
+
+//console.log(index);
+
+
+//let index=12
+
+//12+1=13 +//13+1=14+//27+6=33 + //14-1=13 //33+13=46 //46-2=44
+//13+14=27
+
+//let result=(++index)+ (++index) + 6 +  (--index)  - 2
+
+//console.log(result);
+
+
+//===================Assignment Operator=====================
+
+//let num1=12;
+//num1+=2
+//console.log(num1)
+
+
+//let num1=12;
+//num1-=2
+//console.log(num1)
+
+//let num1=12;
+//num1*=2
+//console.log(num1)
+
+//let num1=12;
+//num1/=2
+//console.log(num1)
+
+
+//=======================Comparison Operator====================
+
+//let num1=12;
+//let num2=10;
+//console.log(num1==num2);  //    it will given answer in boolean (True / False)  
+
+
+
+//let num1=12;
+//let num2=12;
+//console.log(num1==num2);
+
+
+
+
+//================= increament / decreament ======================
+
+// ============= Increament ========================
+
+//let x = 10
+//x = 10+1
+//console.log(x);
+
+//let x = 10
+//x = x+1
+//console.log(x);
+
+
+//let x = 10
+//++x;
+//console.log(x);
+
+//let x = 200
+//++x;   // preincreament
+//console.log(x);
+
+
+//let x = 200
+//x++;   // postincreament
+//console.log(x);
+
+
+//let x = 200
+//x++;
+//console.log(x++);
+
+
+
+//let x = 200
+//++x;
+//console.log(++x);
+
+
+//let x = 200
+//++x;
+//console.log(x++);
+//console.log(x++)
+//console.log(x)
